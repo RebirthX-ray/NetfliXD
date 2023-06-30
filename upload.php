c691379581405eb1935ec35aab566216357dd1f1
RebirthX-ray <138247544+RebirthX-ray@users.noreply.github.com>
2023-06-30 21:58:57 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,15 +2,22 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if a file was uploaded without errors
     if (isset($_FILES["movieFile"]) && $_FILES["movieFile"]["error"] == UPLOAD_ERR_OK) {
+        $allowedExtensions = ["mp4", "mov"]; // Specify the allowed file extensions
         $tempFilePath = $_FILES["movieFile"]["tmp_name"];
         $fileName = $_FILES["movieFile"]["name"];
-        $destination = "uploads/" . $fileName;
+        $fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
+        $destination = "uploads/" . uniqid() . "." . $fileExtension;
 
-        // Move the uploaded file to the desired location
-        if (move_uploaded_file($tempFilePath, $destination)) {
-            echo "File uploaded successfully!";
+        // Check if the file extension is allowed
+        if (in_array($fileExtension, $allowedExtensions)) {
+            // Move the uploaded file to the desired location
+            if (move_uploaded_file($tempFilePath, $destination)) {
+                echo "File uploaded successfully!";
+            } else {
+                echo "Error uploading file.";
+            }
         } else {
-            echo "Error uploading file.";
+            echo "Error: Invalid file extension. Only MP4 and MOV files are allowed.";
         }
     } else {
         echo "Error: Please select a movie file.";
